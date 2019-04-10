d7781a195886af05acdbb63370a31de731fd68b3
deathbreak <1310748243@qq.com>
2019-04-10 13:54:07 +0800

    修改登录拦截器LoginInterceptor

---
+++ src/main/java/com/interceptors/LoginInterceptor.java
@@ -0,0 +1,42 @@
+package com.interceptors;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.bean.Manager;
+
+public class LoginInterceptor implements HandlerInterceptor{
+
+	/**
+	 * µÇÂ¼À¹½ØÆ÷
+	 */
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		Manager fl = (Manager)request.getSession().getAttribute("login");
+		if(fl!=null) { 
+			return true;
+		}else {
+			response.sendRedirect("/drug/index.jsp");
+			//request.getRequestDispatcher("/index.jsp").forward(request, response);
+			return false;
+		}
+		
+	}
+
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {
+		
+	}
+
+	@Override
+	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
+			throws Exception {
+		
+	}
+		
+}
