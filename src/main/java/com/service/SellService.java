4e665990fcee8a102b4dc417bdcfb0b296171559
quq <1310748243@qq.com>
2021-05-13 23:40:11 +0800

    增加利润以及普通店员角色_

--- src/main/java/com/service/SellService.java
+++ src/main/java/com/service/SellService.java
@@ -6,7 +6,8 @@ import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 
-
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -26,16 +27,16 @@ import com.utils.*;
 
 @Service
 public class SellService {
-		
+
 	@Autowired
 	SellMapper sell;
-	
+
 	@Autowired
 	StoreMapper store;
-	
+
 	@Autowired
 	RxdrugMapper rdm;
-	
+
 	@Autowired
 	RxpersonMapper rpm;
 	public List<Sell> GetAllSellService(){
@@ -43,9 +44,9 @@ public class SellService {
 	}
 
 	public void DeleteSellService(String drugname, String changshang,
-			String pihao, String amount) {
+								  String pihao, String amount) {
 		sell.DeleteSellByDCPA(drugname, changshang, pihao, amount);
-		
+
 	}
 
 	public List<Store> ForSellSelectService(String qd) {
@@ -64,44 +65,44 @@ public class SellService {
 	}
 
 	public List<Store> QueryDCPinStoreService(String drugname, String changshang,
-			String pihao) {
+											  String pihao) {
 		return store.QueryByNCP(drugname, changshang, pihao);
 	}
 
 	public void ProSellService(String drugname, String changshang,
-			String pihao, String amount) {
-		
-			List<Store> sssss = store.QueryByNCP(drugname, changshang, pihao);
-			
-			if(!sssss.isEmpty()){
-				Store xs = sssss.get(0);
-				List<Sell> checksell = sell.QuerySellByDCP(drugname, changshang, pihao);
-				int flag = checksell.size();
-				if(flag<=0){
-					String sum = "";
-					sum=StringPro.mul(xs.getPrice(),amount);
-					Sell as = new Sell(xs.getDrugname(),xs.getChangshang(),xs.getPrice(), xs.getDate(),xs.getPihao(),xs.getBeizhu(), xs.getCount(), xs.getUnit(), xs.getGuige(), amount, sum);
-					sell.AddSell(as);
-				}else{
-					Sell dd = checksell.get(0);
-					String newamount=StringPro.add(dd.getAmount(),amount);
-                    String sum2=StringPro.mul(newamount,dd.getPrice());
-                    sell.UpdateSellCountSum(newamount, sum2, drugname, changshang, pihao);
-				}
+							   String pihao, String amount) {
+
+		List<Store> sssss = store.QueryByNCP(drugname, changshang, pihao);
+
+		if(!sssss.isEmpty()){
+			Store xs = sssss.get(0);
+			List<Sell> checksell = sell.QuerySellByDCP(drugname, changshang, pihao);
+			int flag = checksell.size();
+			if(flag<=0){
+				String sum = "";
+				sum=StringPro.mul(xs.getPrice(),amount);
+				Sell as = new Sell(xs.getDrugname(),xs.getChangshang(),xs.getPrice(), xs.getDate(),xs.getPihao(),xs.getBeizhu(), xs.getCount(), xs.getUnit(), xs.getGuige(), amount, sum);
+				sell.AddSell(as);
+			}else{
+				Sell dd = checksell.get(0);
+				String newamount=StringPro.add(dd.getAmount(),amount);
+				String sum2=StringPro.mul(newamount,dd.getPrice());
+				sell.UpdateSellCountSum(newamount, sum2, drugname, changshang, pihao);
 			}
-			
+		}
+
 	}
 
 	public List<Rxdrug> SellitService() {
 		List<Sell> updatesell = sell.GetAllSell();
 		if(!updatesell.isEmpty()){
-			
-			
+
+
 			SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");//设置日期格式
-            String date2=df.format(new Date());
-            List<Rxdrug> CheckRxdrug = new ArrayList<Rxdrug>();
+			String date2=df.format(new Date());
+			List<Rxdrug> CheckRxdrug = new ArrayList<Rxdrug>();
 			for (Sell udsell : updatesell) {
-				
+
 				if(udsell.getBeizhu().equals("处方药")) {
 					Rxdrug rd = new Rxdrug(date2,udsell.getDrugname(),udsell.getChangshang(),udsell.getDate(),udsell.getPihao(),udsell.getAmount());
 					rdm.AddRxdrug(rd);
@@ -113,8 +114,8 @@ public class SellService {
 				store.UpdateStoreCount(newcount, udsell.getDrugname(), udsell.getChangshang(), udsell.getPihao());
 				if(newcount.equals("0")){
 					sell.AddSellover(new Sellover(udsell.getDrugname(), udsell.getChangshang(), udsell.getPrice(), udsell.getDate(), udsell.getPihao(), udsell.getBeizhu(), newcount, udsell.getUnit(), udsell.getGuige()));
-                    store.DeleteCountZero();
-                }
+					store.DeleteCountZero();
+				}
 			}
 			sell.DelTableSell();
 			if(CheckRxdrug.size()>0) {
@@ -122,7 +123,7 @@ public class SellService {
 			}else {
 				return null;
 			}
-			
+
 		}else {
 			return null;
 		}
@@ -140,14 +141,35 @@ public class SellService {
 			p.printSheet("0001","xxx药店","张三",sum,"现金","普通会员","7xxxxx3","xxx大桥边",check);
 		}
 	}
-	//倒序输出结果
-	public List<Sellrecords> GetRecordsService(String selltime){
-		if(selltime.equals("")||selltime==null){
+	//	//倒序输出结果
+//	public List<Sellrecords> GetRecordsService(String selltime){
+//		if(selltime.equals("")||selltime==null){
+//			List<Sellrecords> re = sell.GetAllSellrecords();
+//			Collections.reverse(re);
+//			return re;
+//		}else{
+//			return sell.GetRecordsByST(selltime);
+//		}
+//	}
+	//update this function
+	public PageInfo<Sellrecords> GetRecordsService(Integer pn, String selltime){
+		if(selltime.equals("nowtime")){
+			SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+			selltime = sdf.format(new Date());//获取系统当前时间
+		}
+
+		if(selltime.equals("")||selltime==null) {
+			PageHelper.startPage(pn, 8);
 			List<Sellrecords> re = sell.GetAllSellrecords();
 			Collections.reverse(re);
-			return re;
-		}else{
-			return sell.GetRecordsByST(selltime);
+			PageInfo<Sellrecords> page_1 = new PageInfo<Sellrecords>(re,5);
+			return page_1;
+		}else {
+			PageHelper.startPage(pn, 8);
+			List<Sellrecords> re2 = sell.GetRecordsByST(selltime);
+			Collections.reverse(re2);
+			PageInfo<Sellrecords> page_2 = new PageInfo<Sellrecords>(re2,5);
+			return page_2;
 		}
 	}
 	//time
@@ -159,11 +181,11 @@ public class SellService {
 			return selltime;
 		}
 	}
-	//
+	//算净利润
 	public String GetMoneyInfo(String selltime){
 		if(selltime.equals("nowtime")){
 			SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-			selltime = sdf.format(new Date());//
+			selltime = sdf.format(new Date());//获取系统当前时间
 		}
 		if (selltime.equals("") || selltime == null) {
 			List<Sellrecords> re = sell.GetAllSellrecords();
@@ -184,20 +206,20 @@ public class SellService {
 		return sum;
 	}
 	public void DelRecordsService(String drugname, String changshang,
-			String pihao, String selltime) {
+								  String pihao, String selltime) {
 		sell.DeleteSellrecords(drugname, changshang, pihao, selltime);
-		
+
 	}
 	public List<Sellover> GetASService(){
 		return sell.GetAllSellover();
 	}
 
 	public void DelSelloverService(String drugname, String changshang,
-			String pihao) {
+								   String pihao) {
 		sell.DelSellover(drugname, changshang, pihao);
 	}
 	/**
-	 * 
+	 *
 	 * @return 未记录的处方药条数
 	 */
 	public Integer RxCountService() {
@@ -205,13 +227,13 @@ public class SellService {
 	}
 
 	public String AddRxPersonService(Rxperson rx) {
-			List<Rxperson> check = rpm.GetByTime(rx.getTime());
-			if(check.size()==0) {
-				rpm.AddRxperson(rx);
-				return "添加处方药信息成功";
-			}else {
-				return "添加失败,已经添加了这个处方药信息";
-			}
+		List<Rxperson> check = rpm.GetByTime(rx.getTime());
+		if(check.size()==0) {
+			rpm.AddRxperson(rx);
+			return "添加处方药信息成功";
+		}else {
+			return "添加失败,已经添加了这个处方药信息";
+		}
 	}
 }
 
